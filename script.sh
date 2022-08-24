9d009801d061821150de13b613215d6ecd4a5583
Nicolai von Oppeln-Bronikowski <57228182+nbronikowski@users.noreply.github.com>
2022-08-24 16:38:30 -0230

    working base version

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 MISSIONS=$PWD/sunfish/labsea_2021/realtime
 SCRIPTS=$PWD
 
-cd ${MISSIONS}/from-glider
+cd ${MISSIONS}/
 mkdir ascii nc 2>/dev/null
 
 ##  CONVERT BINARY TO TXT
-cd ${MISSIONS}/from-glider/raw
+cd ${MISSIONS}/raw
 ln -s ../cache .
 for f in ${glider}*bd; do
     if [[ ! -e ../ascii/$f.ascii ]]; then
@@ -15,7 +15,7 @@ done
 rm cache
 
 ##  CONVERT TO NC
-cd ${MISSIONS}/from-glider/ascii
+cd ${MISSIONS}/ascii
 for f in $(ls | sed 's/\..bd\.ascii//' | sort -u); do
     if [[ ! -e ../nc/$f.nc ]]; then
         python3 ${SCRIPTS}/bd2nc.py $f
@@ -23,5 +23,5 @@ for f in $(ls | sed 's/\..bd\.ascii//' | sort -u); do
 done
 
 ##  CREATE 1 NC FILE FOR THE WHOLE MISSION
-cd ${MISSIONS}/from-glider/ascii
+cd ${MISSIONS}/ascii
 python3 ${SCRIPTS}/1nc.py
