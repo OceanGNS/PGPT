cc69a85b27a4060aad0ac28791e808d0e226e483
TaimazB <taimaz@gmail.com>
2022-12-02 08:58:49 -0330

    Global attributes partially automated.

--- process_glider_deployment_sunfish_2021-12-03_data_DT.sh
+++ process_glider_deployment_sunfish_2021-12-03_data_DT.sh
@@ -1,9 +1,12 @@
+MAIN=$PWD
 glider=sunfish
-MISSION=$PWD/glider_data/${glider}/sunfish_20211203/realtime
-SCRIPTS=$PWD/glider_processing_scripts
+MISSION=${MAIN}/glider_data/${glider}/sunfish_20211203/realtime
+SCRIPTS=${MAIN}/glider_processing_scripts
+metaFile=${MAIN}/metadata_glider_deployment_sunfish_2021-12-03_data_DT.txt
+
 
 cd ${MISSION}/
-mkdir txt nc 2>/dev/null
+mkdir txt nc nc_ioos 2>/dev/null
 
 ##  CONVERT BINARY TO TXT
 cd ${MISSION}/raw
@@ -15,20 +18,21 @@ ln -s ../cache .
 #done
 rm cache
 
-
 ###################  REALTIME  ###################
 ##  CONVERT TO NC
 cd ${MISSION}/txt
 for f in $(ls ${glider}*.[st]bd.txt | sed 's/\..bd\.txt//' | sort -u); do
     if [[ ! -e ../nc/$f.nc ]]; then
         python3 ${SCRIPTS}/realtime2nc.py $f
+
+        # ADD ATTRIBUTES
+        bash ${SCRIPTS}/addAttrs.sh ${metaFile} ../nc/${f}_realtime.nc ../nc_ioos/${f}_realtime.nc
     fi
 done
 
 ##  CREATE 1 NC FILE FOR THE WHOLE MISSION
 cd ${MISSION}/txt
 python3 ${SCRIPTS}/ncTimeseries.py realtime st
-
 
 ###################  DELAYED  ###################
 ##  CONVERT TO NC
